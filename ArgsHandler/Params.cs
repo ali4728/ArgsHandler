a8d7f58cfaf99640cc85f634c3b5850518cce6f3
ali4728 <erkan.ali.yilmaz@gmail.com>
2015-11-07 20:12:33 -0800

    Added Params and CmdLine Classes

---
+++ ArgsHandler/Params.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace ArgsHandler
+{
+    public class Params
+    {
+       
+        public static string InputFolder { get; set; }        
+        public static string OutputFolder { get; set; }
+        public static string FindReplace { get; set; }
+        
+
+        public static void parseAppConfig()
+        {
+            var vCustomParameters = ConfigurationManager.GetSection("CustomParameters") as NameValueCollection;
+            if (vCustomParameters != null)
+            {
+                foreach (var pKey in vCustomParameters.AllKeys)
+                {
+                    string pValue = vCustomParameters.GetValues(pKey).FirstOrDefault();
+                    if ("InputFolder".Equals(pKey)) { Params.InputFolder = pValue; }                    
+                    else if ("OutputFolder".Equals(pKey)) { Params.OutputFolder = pValue; }
+                    else if ("FindReplace".Equals(pKey)) { Params.FindReplace = pValue; }
+                }
+            }
+
+            if (String.IsNullOrEmpty(Params.InputFolder))
+            {
+                Console.WriteLine("parseAppConfig() - Missing InputFolder parameter");
+                throw new Exception("Missing arguments in App.config");
+            }
+        }
+
+        public static void parseCmdArgs(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                parseAppConfig();
+                return;
+            }
+            else
+            {
+                parseAppConfig();
+                ConsoleCmdLine console = new ConsoleCmdLine();
+
+                CmdLineString _inputFolder = new CmdLineString("i", true, "Specify Input Folder");
+                CmdLineString _outputFolder = new CmdLineString("o", true, "Specify Output Folder");
+                CmdLineString _findReplace = new CmdLineString("f", false, "Specify Find Replace String in deliter sets of @@ and || ex: left||right@@l2||r2@@l3||r3");
+                
+                console.RegisterParameter(_inputFolder);
+                console.RegisterParameter(_outputFolder);
+                console.RegisterParameter(_findReplace);
+                console.Parse(args);
+
+                InputFolder = _inputFolder;
+                OutputFolder = _outputFolder;
+                FindReplace = _findReplace;
+             
+            }
+
+
+        }
+
+
+        //public static void ConfigLogging()
+        //{
+
+        //    FileAppender fileAppender = new FileAppender();
+        //    fileAppender.AppendToFile = true;
+        //    fileAppender.LockingModel = new FileAppender.MinimalLock();
+        //    fileAppender.File = @"C:\temp\Logfile.log";
+
+        //    log4net.Layout.PatternLayout pl = new log4net.Layout.PatternLayout();
+        //    //d:timestamp t:thread p:level c:Class name m:message n:new line
+        //    //pl.ConversionPattern = "%d [%2%t] %-5p [%-10c] %m%n";
+        //    pl.ConversionPattern = "%d|%-5p|[%-10c]|%m%n";
+        //    pl.ActivateOptions();
+
+        //    fileAppender.Layout = pl;
+        //    fileAppender.ActivateOptions();
+
+        //    log4net.Config.BasicConfigurator.Configure(fileAppender);
+
+        //}
+    }
+
+}
--- ArgsHandler/Program.cs
+++ ArgsHandler/Program.cs
@@ -11,24 +11,45 @@ namespace ArgsHandler
         static void Main(string[] args)
         {
 
-            Args.Register("in", new Arg(true, "-in Specifies folder where the input file is located ex: -in \"C:\\inputfile.txt\""));
-            Args.Register("out", new Arg(true, "-out Specifies output folder ex: -out \"C:\\somepath\""));
+            /*
+             * Command line arg example
+             * -i C:\temp\infolder -o C:\temp\outfolder -f "left||right@@l2||r2@@l3||r3"
+             */
 
-            try
+            Params.parseCmdArgs(args);
+            Console.WriteLine("InputFolder: " + Params.InputFolder);
+            Console.WriteLine("InputFolder: " + Params.OutputFolder);
+
+            String[] _outer = Params.FindReplace.Split(new string[] { "@@" }, StringSplitOptions.None);
+            foreach (String item in _outer)
             {
-                bool help = false;
-                Args.Parse(args, out help);
-
-                if (help)
+                String [] _innter = item.Split(new string[] { "||" }, StringSplitOptions.None);
+                foreach (String it in _innter)
                 {
-                    return;
+                    Console.WriteLine(it);
                 }
             }
-            catch (Exception)
-            {
-                Environment.Exit(160);
+            Console.WriteLine("Program has completed. Pres enter to exit.");
+            Console.ReadLine();
 
-            }
+            //Args.Register("in", new Arg(true, "-in Specifies folder where the input file is located ex: -in \"C:\\inputfile.txt\""));
+            //Args.Register("out", new Arg(true, "-out Specifies output folder ex: -out \"C:\\somepath\""));
+
+            //try
+            //{
+            //    bool help = false;
+            //    Args.Parse(args, out help);
+
+            //    if (help)
+            //    {
+            //        return;
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    Environment.Exit(160);
+
+            //}
             
         }
     }
